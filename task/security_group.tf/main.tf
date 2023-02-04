a699203781104b9fe6395b6105c1e5ee12a92c2e
oksanaivashko <oksivashko13@gmail.com>
2023-02-04 15:26:19 -0600

    added security_group.tf

--- task/security_group.tf/main.tf
+++ task/security_group.tf/main.tf
@@ -1,48 +1,48 @@
-resource "aws_security_group" "allow_tls" {
-  name        = "instance-sg"
-  description = "Allow traffic for ec2_instance"
+resource "aws_security_group" "Task" {
+  name        = "main"
+  description = "Allow inbound traffic"
   vpc_id      = "vpc-0a42426aa52e0c8f9"
 
-ingress = [
-    {
-      description      = "HTTPS from VPC"
-      from_port        = 443
-      to_port          = 443
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-    },
-    {
-      description      = "HTTP from VPC"
-      from_port        = 80
-      to_port          = 80
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-    
-    },
-    {
-      description      = "SSH from VPC"
-      from_port        = 22
-      to_port          = 22
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-    },
-    {
-      description      = "open from VPC"
-      from_port        = 3306
-      to_port          = 3306
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-    }
-  ]
+  ingress {
+    description      = "Allow SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.main.cidr_block]
+  }
+
+  ingress {
+    description      = "Allow TCP form VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.main.cidr_block]
+  }
 
 
-  egress = [
-    {
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-    }
-  ]
+  ingress {
+    description      = "Allow HTTPS form VPC"
+    from_port        = 433
+    to_port          = 433
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.main.cidr_block]
+  }
+ingress {
+    description      = "Allow RDS port form VPC"
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.main.cidr_block]
+  }
+
+
+
+
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
 }
