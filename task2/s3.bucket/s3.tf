ede9bcd7002412fa76212d61794333eadcec98b7
oksanaivashko <oksivashko13@gmail.com>
2023-02-04 17:32:21 -0600

    added file

--- task2/s3.bucket/s3.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "mybucket" {
-  bucket = "terraform13-s3-bucket"
-}
-
-resource "aws_s3_bucket_accelerate_configuration" "example" {
-  bucket = aws_s3_bucket.mybucket.bucket
-  status = "Enabled"
-}
