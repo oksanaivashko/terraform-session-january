10ba2a7b4a4810c8abdee2269417708e99661ef0
oksanaivashko <oksivashko13@gmail.com>
2023-02-04 12:12:58 -0600

    added versions.tf

--- session2/versions.tf
+++ session2/versions.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = "~> 0.14.0"# Terraform version 
+  required_version = "~> 1.3.0"# Terraform version 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.24.0" # AWS Provider version
+      version = "~> 4.53.0" # AWS Provider version
     }
   }
 }
