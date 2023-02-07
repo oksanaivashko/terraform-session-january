02e19da1c056d2c39ad75e85b16e26d0a97bf0b0
oksanaivashko <oksivashko13@gmail.com>
2023-02-07 17:35:27 -0600

    addded vpc.tf

--- task3/VPC/vpc.tf
+++ task3/VPC/vpc.tf
@@ -10,3 +10,6 @@ resource "aws_vpc" "task_vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 }
+tags = {                               #tags {} = expecting a map 
+    Name = task_vpc
+  }
