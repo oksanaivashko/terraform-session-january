02007622a3dfb02a0b7d1d368bfaabf30102827d
oksanaivashko <oksivashko13@gmail.com>
2023-02-07 15:49:42 -0600

    public_subnets.tf

--- task3/VPC/public_subnets.tf
+++ task3/VPC/public_subnets.tf
@@ -48,7 +48,7 @@ resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.task_vpc.id
 
   route {
-    cidr_block = "10.0.1.0/16"
+    cidr_block = "10.0.0.0/16"
     gateway_id = aws_internet_gateway.int_gway.id
   }
 }
