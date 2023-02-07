4e10e36e3060934ac70626aa4fe2c76dd68888f3
oksanaivashko <oksivashko13@gmail.com>
2023-02-06 21:15:46 -0600

    added session3

--- session3/ec2.tf
+++ session3/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "first_ec2" {     #first label predifined by terraform and second label by author
-  ami           = "ami-06e85d4c3149db26a" #arguments (key and value)
-  instance_type = var.instance_type
+  ami           = var.ami #arguments (key and value)
+  instance_type = var.ami
   vpc_security_group_ids = [aws_security_group.main_sg.id] #security id, if we dont know the security id we use reference to resource(reference to resource)
   tags = {                               #tags {} = expecting a map 
     Name        = var.name 
