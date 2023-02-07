2da8fd357d0def77a6e0a3a3d33fab00ccb8aad7
oksanaivashko <oksivashko13@gmail.com>
2023-02-06 20:35:35 -0600

    added session3

--- session3/ec2.tf
+++ session3/ec2.tf
@@ -2,9 +2,8 @@ resource "aws_instance" "first_ec2" { #first label predifined by terraform and s
   ami           = "ami-06e85d4c3149db26a" #arguments (key and value)
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.main_sg.id] #security id, if we dont know the security id we use reference to resource(reference to resource)
-  tags = {
-    Name        = "first"
-    Environment = "dev"
+  tags = {                   #tags {} = expecting a map 
+    Name        = "development"    
   }
 }
 
@@ -25,7 +24,11 @@ resource "aws_instance" "first_ec2" { #first label predifined by terraform and s
 #Referrence to Resources 
 #first_label.second_label.attribute
 #first_label and the second label "aws_security_group.main_sg.id"
-#attribute knows after createn 
+
+#Attribute knows after createn 
 #whenever you use reference you don't use ""
 #"aws_security_group.main_sg.id" = static or hard coder calue (string)
 #aws_security_group.main_sg.id = referrence to resource (string, number, booleun)
+
+#Syntax
+#the entire syntax expect {} a map key and value
