aa7c5bca99b9dd721f52b92dbddbbf7751e1f632
oksanaivashko <oksivashko13@gmail.com>
2023-02-06 20:06:06 -0600

    added session3

--- session3/ec2.tf
+++ session3/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "first_ec2" { #first label predifined by terraform and second label by author
   ami           = "ami-06e85d4c3149db26a" #arguments (key and value)
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["aws_security_group.main_sg.id"] #security id, if we dont know the security id we use reference to resource(reference to resource)
+  vpc_security_group_ids = [aws_security_group.main_sg.id] #security id, if we dont know the security id we use reference to resource(reference to resource)
   tags = {
     Name        = "first"
     Environment = "dev"
