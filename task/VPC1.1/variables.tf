2dce232d954b3b818fe36b74a744efd51213fcef
oksanaivashko <oksivashko13@gmail.com>
2023-02-09 15:32:16 -0600

    added vpc_1.1.tf

--- task/VPC1.1/variables.tf
+++ task/VPC1.1/variables.tf
@@ -3,11 +3,12 @@ variable "vpc_name" {
     description = "vpc name"
     default = "home_task"
 }
-variable "cidr_block" {
-    type = string
-    description = "specify cidr block"
-    default = "10.0.0.0/16"
+variable "vpc_cidr_block" {
+  type = string
+  description = "specify CIDR block for VPC"
+  default = "10.0.0.0/16"
 }
+
 variable "vpc_tags" {
     type = string
     description = "gives tags to my vpc"
