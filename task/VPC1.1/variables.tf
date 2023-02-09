22398f8f6344aff15a7d7bb7cc4b474379e91473
oksanaivashko <oksivashko13@gmail.com>
2023-02-09 17:10:50 -0600

    added vpcz_v1.1

--- task/VPC1.1/variables.tf
+++ task/VPC1.1/variables.tf
@@ -1,6 +1,6 @@
 variable "vpc_name" {
     type = string
-    description = "vpc name"
+    description = "name for vpc"
     default = "task_vpc"
 }
 variable "vpc_cidr_block" {
@@ -8,3 +8,31 @@ variable "vpc_cidr_block" {
   description = "specify CIDR block for VPC"
   default = "10.0.0.0/16"
 }
+#variable "us-west-2a" {
+#    type = string
+#    description = "specify az"
+#    default = ""
+#}
+##subnets
+#task_vpc_subnets = {
+# "a" = {
+#   cidr = "10.0.1.0/26"
+#   az   = "eu-central-1a"
+# },
+# "b" = {
+#   cidr = "10.0.2.0/26"
+#   az   = "eu-central-1a"
+# },
+# "c" = {
+#   cidr = "10.0.3.0/26"
+#   az   = "eu-central-1b"
+# },
+# "d" = {
+#   cidr = "10.0.4.0/26"
+#   az   = "eu-central-1c"
+# },
+# "e" = {
+#   cidr = "10.0.5.0/26"
+#   az   = "eu-central-1b"
+# }
+#}
--- task/VPC1.1/vpc_v1.1.tf
+++ task/VPC1.1/vpc_v1.1.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "task_vpc" {       
   name             = var.vpc_name
-  cidr_blocks      = var.cidr_block
+  cidr_block      = var.cidr_block
   instance_tenancy = "default"
   tags = {
     Name = var.vpc_name
