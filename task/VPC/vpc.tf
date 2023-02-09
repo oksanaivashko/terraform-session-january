45c75987a4115147b0c10c2cfc90ac5da121d6bf
oksanaivashko <oksivashko13@gmail.com>
2023-02-09 15:33:13 -0600

    added vpc_1.1.tf

--- task/VPC/vpc.tf
+++ task/VPC/vpc.tf
@@ -7,7 +7,7 @@
 
 resource "aws_vpc" "task_vpc" {       
   name             = var.vpc_name
-  cidr_block       = var.cidr_block
+  cidr_blocks      = var.cidr_block
   instance_tenancy = "default"
   tags = {
     name = var.vpc_tags
