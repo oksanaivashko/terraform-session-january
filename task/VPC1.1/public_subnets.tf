1a1982dd4a558df3e258f87c9140dadb57fa8ff2
oksanaivashko <oksivashko13@gmail.com>
2023-02-09 18:57:32 -0600

    final commit for vpc_v1.1

--- task/VPC1.1/public_subnets.tf
+++ task/VPC1.1/public_subnets.tf
@@ -1,13 +1,5 @@
 #Create subnets - public 
 
-#Choose VPC 
-#Subnet name  
-#Availability zone = choose each subnet in the different zone 
-#IPv4 CIDR block = 10.0.1.0/24 
-
-#To make these subnets public we need to atach int-gateway
-
-
 resource "aws_subnet" "public_a" {
     vpc_id = aws_vpc.task_vpc.id
   availability_zone = var.public_subnet_a
@@ -25,18 +17,13 @@ resource "aws_subnet" "public_c" {
   cidr_block       = var.public_subnet_cidr_block_c
 }
 
-
 #Create Internet Gateway and attach it to the Public-Route-Table 
-#Route table to connect internet you need edit “subnet associate” drop down and add it 
 
 resource "aws_internet_gateway" "int_gway" {
   vpc_id = aws_vpc.task_vpc.id
 }
 
 #Create a route table - public 
-#Choose VPC 
-#Name for route table
-#Attach to the int gateway
 
 resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.task_vpc.id
