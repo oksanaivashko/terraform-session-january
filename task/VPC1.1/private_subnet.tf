7e78c7363e77a2a316849104ddab2f9162ada132
oksanaivashko <oksivashko13@gmail.com>
2023-02-09 18:47:53 -0600

    final commit for vpc_v1.1

--- task/VPC1.1/private_subnet.tf
+++ task/VPC1.1/private_subnet.tf
@@ -1,11 +1,5 @@
 #Create subnets - private 
 
-#Choose VPC 
-#Subnet name  
-#Availability zone = choose or no preference  
-#IPv4 CIDR block = 10.0.11.0/24 = always has a 256 Ips 
-#Attach to NAT gateway
-
 resource "aws_subnet" "private_a" {
     vpc_id = aws_vpc.task_vpc.id
   availability_zone = var.private_subnet_a
@@ -26,21 +20,12 @@ resource "aws_subnet" "private_c" {
 
 #Create NAT gateway  
 
-#Name tag 
-#Select a subnet in which to create the NAT gateway = only from public subnet 
-#Connectivity type = public 
-#Elastic IP allocation ID = allocate 
-#To connect subnets to route table you need go route table > edit subnets > add private subnets only 
-
 resource "aws_nat_gateway" "nat_gway" {
   connectivity_type = "private"
   subnet_id         = aws_subnet.public_a.id
 }
 
 #Create a route table - private 
-#
-#Name for route table 
-#Choose VPC 
 
 resource "aws_route_table" "tasktest_private_route_table" {
   vpc_id = aws_vpc.task_vpc.id
