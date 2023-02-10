08dac8b3eac23fff80eab3d89339fa11e2dfc9ac
oksanaivashko <oksivashko13@gmail.com>
2023-02-10 16:06:00 -0600

    testing public subnet

--- task/vpc2.1/public_subnets_v2.tf
+++ task/vpc2.1/public_subnets_v2.tf
@@ -2,7 +2,7 @@
 
 resource "aws_subnet" "public_subnet" {
     vpc_id = aws_vpc.task_vpc.id
-    az = var.public_subnet_az
+    az = var.public_subnet_az[0]
   cidr  = var.public_subnet_cidr_block[0]
 }
 
