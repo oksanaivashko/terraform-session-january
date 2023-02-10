37f89d5a4f3451f5c8696ba8128f5eda7793983e
oksanaivashko <oksivashko13@gmail.com>
2023-02-09 18:44:59 -0600

    added private route table

--- task/VPC1.1/private_subnet.tf
+++ task/VPC1.1/private_subnet.tf
@@ -46,7 +46,7 @@ resource "aws_route_table" "tasktest_private_route_table" {
   vpc_id = aws_vpc.task_vpc.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block = var.public_route_table_cidr
     nat_gateway_id = aws_nat_gateway.nat_gway.id
   }
 }
