49311691cc01e5970736a27213981e938def7abb
oksanaivashko <oksivashko13@gmail.com>
2023-02-10 11:21:56 -0600

    added vpc_v2.1

---
+++ task/vpc2.1/public_subnets_v2.tf
@@ -0,0 +1,36 @@
+#   --- Create Subnets - Public ---
+
+resource "aws_subnet" "public_a" {
+    vpc_id = aws_vpc.task_vpc.id
+  availability_zone = var.my_subnets
+  cidr_block       = var.cidr_subnets
+}
+
+#resource "aws_subnet" "public_b" {
+#    vpc_id = aws_vpc.task_vpc.id
+#  availability_zone = var.public_subnet_b
+#  cidr_block       = var.public_subnet_cidr_block_b
+#}
+#resource "aws_subnet" "public_c" {
+#    vpc_id = aws_vpc.task_vpc.id
+#  availability_zone = var.public_subnet_c
+#  cidr_block       = var.public_subnet_cidr_block_c
+#}
+#
+##    --- Create Internet Gateway and attach TO VPC ---
+#
+#resource "aws_internet_gateway" "int_gway" {
+#  vpc_id = aws_vpc.task_vpc.id
+#}
+#
+##   --- Create a Route Table - Public ---
+#
+#resource "aws_route_table" "public_route_table" {
+#  vpc_id = aws_vpc.task_vpc.id
+#
+#  route {
+#    cidr_block = var.public_route_table_cidr
+#    gateway_id = aws_internet_gateway.int_gway.id
+#  }
+#}
+#
---
+++ task/vpc2.1/vpc_v2.1.tf
@@ -0,0 +1,8 @@
+#   --- Crete my VPC ---
+resource "aws_vpc" "task_vpc" {       
+  cidr_block      = var.vpc_cidr_block
+  instance_tenancy = "default"
+  tags = {
+    Name = var.vpc_name
+  }
+}
