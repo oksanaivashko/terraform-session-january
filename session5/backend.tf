192fcafc02ffe7df2ece28bba08249b7fdec01cc
oksanaivashko <oksivashko13@gmail.com>
2023-02-11 11:10:11 -0600

    added s3

--- session5/backend.tf
+++ session5/backend.tf
@@ -1,7 +1,7 @@
 terraform {
  backend "s3" {
     bucket = "terraform-session-september-backend-oksana"
-    region = "us-west-12"
+    region = "us-west-2"
     key    = "session-5/terraform.tfstate"  #where does terraform need to store your file.Path or
   }
 }
