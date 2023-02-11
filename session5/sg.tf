42129b0bf4b2a7a1ff5b8a0ac2f95cb716bfa538
oksanaivashko <oksivashko13@gmail.com>
2023-02-11 12:43:42 -0600

    added sg_rule

--- session5/sg.tf
+++ session5/sg.tf
@@ -8,10 +8,9 @@ resource "aws_security_group_rule" "ingress" {
   to_port           = element( var.ports, 0) 
   protocol          = "-tcp"
   from_port         = element( var.ports, 0) 
-  cidr_blocks = [""]
+  cidr_blocks = ["0.0.0.0/0"]
   security_group_id = aws_security_group.main_sg.id
 }
-
 resource "aws_security_group_rule" "egress" {
   type              = "egress"
   to_port           = 0
