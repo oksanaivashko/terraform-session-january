58da855f9b0ab90889058ab4c5fb49a9bc0358c5
oksanaivashko <oksivashko13@gmail.com>
2023-02-10 18:37:24 -0600

    fixed error

--- task/vpc2.1/private_subnet_v2.tf
+++ task/vpc2.1/private_subnet_v2.tf
@@ -24,7 +24,7 @@ resource "aws_route_table" "private_route_table" {
   vpc_id = aws_vpc.task_vpc.id
 
   route {
-    cidr_block = var.public_route_table_cidr[0]
+    cidr_block = var.public_route_table
     nat_gateway_id = aws_nat_gateway.nat_gway.id
   }
 }
