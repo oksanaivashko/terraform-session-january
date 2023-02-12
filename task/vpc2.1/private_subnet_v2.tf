064c6665085a0ce302be96a134534afd1b7557e2
oksanaivashko <oksivashko13@gmail.com>
2023-02-11 18:27:11 -0600

    testing r_t

--- task/vpc2.1/private_subnet_v2.tf
+++ task/vpc2.1/private_subnet_v2.tf
@@ -24,7 +24,7 @@ resource "aws_route_table" "private_route_table" {
   vpc_id = aws_vpc.task_vpc.id
 
   route {
-    cidr_block = var.private_route_table
+    #cidr_block = var.private_route_table
     nat_gateway_id = aws_nat_gateway.nat_gway.id
   }
 }
