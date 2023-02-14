136d4c81cd33f651fd816d30f6fce9a8b9e3db04
oksanaivashko <oksivashko13@gmail.com>
2023-02-13 19:03:09 -0600

    added y=two env

--- session6/filestructure/main.tf
+++ session6/filestructure/main.tf
@@ -1,6 +1,11 @@
 resource "aws_sqs_queue""main"{
     count = 2
-    name = "${var.env}-sqs"
+    #name = "${var.env}-sqs"
+    #Name1       = format("%s-instance", var.env)
+    tags = {
+        Environment = var.env [0]
+        Name = format("%s-sqs", var.env)
+    }
     tags = {
         Environment = var.env [1]
         Name = format("%s-sqs", var.env)
