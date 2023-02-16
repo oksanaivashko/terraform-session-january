a88d65f15d497afbcb8d9234fc1f337499d8923e
oksanaivashko <oksivashko13@gmail.com>
2023-02-16 15:31:38 -0600

    adding tags to the public subnets, int gateway and route table

--- task/vpc_1.3/public_subnets_v1.3.tf
+++ task/vpc_1.3/public_subnets_v1.3.tf
@@ -4,18 +4,21 @@ resource "aws_subnet" "public_subnet_a" {
   vpc_id            = aws_vpc.task_vpc.id
   availability_zone = var.availability_zone[0]
   cidr_block        = var.cidr_block[0]
+  tags = local.common_tags
 }
 
 resource "aws_subnet" "public_subnet_b" {
   vpc_id            = aws_vpc.task_vpc.id
   availability_zone = var.availability_zone[1]
   cidr_block        = var.cidr_block[1]
+  tags = local.common_tags
 }
 
 resource "aws_subnet" "public_subnet_c" {
   vpc_id            = aws_vpc.task_vpc.id
   availability_zone = var.availability_zone[2]
   cidr_block        = var.cidr_block[2]
+  tags = local.common_tags
 }
 
 
@@ -23,6 +26,7 @@ resource "aws_subnet" "public_subnet_c" {
 
 resource "aws_internet_gateway" "int_gway" {
   vpc_id = aws_vpc.task_vpc.id
+  tags = local.common_tags
 }
 
 #   --- Create a Route Table - Public ---
@@ -33,5 +37,6 @@ resource "aws_route_table" "public_route_table" {
   route {
     cidr_block = var.cidr_bloc_public_route_table
     gateway_id = aws_internet_gateway.int_gway.id
+    tags = local.common_tags
   }
 }
