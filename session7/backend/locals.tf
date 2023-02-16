41d7b63fc71653a32071d9de00db1adce14ace0e
oksanaivashko <oksivashko13@gmail.com>
2023-02-15 19:51:03 -0600

    addded locals.tf

---
+++ session7/backend/locals.tf
@@ -0,0 +1,24 @@
+locals {
+    name = "aws-${var.team}-${var.env}-${var.app}-resource-${var.index}"
+    common_tags = {
+        Environment = var.env
+        Team = var.app
+        App = var.app
+        manage_by = var.manage_by
+        Owner = var.owner
+    }
+}
+
+
+#providers-team-env-app-resource-index = naming standard
+
+# aws-cloud-dev-jerry-loadbalancer-0 
+
+# Common Tags:
+
+# 1. Name = aws-cloud-dev-jerry-loadbalancer-0
+# 2. Environment = Dev
+# 3. Team = Cloud 
+# 4. App  = Jerry
+# 5. Managed_By = "Terraform, Cloudformation, Python, Manual"
+# 6. Owner = "Kris"
---
+++ session7/variables.tf
@@ -0,0 +1,28 @@
+variable "env" {
+  type = string
+  default = "dev"
+}
+
+variable "team" {
+    type = string
+    default = "devops"
+}
+
+variable "app" {
+    type = string
+    default = "shera"
+}
+variable "index" {
+    type = string
+    default = "0"
+}
+
+variable "manage_by" {
+    type = string
+    default = "terraform"
+}
+
+variable "owner" {
+    type = string
+    default = "oksana"
+}
