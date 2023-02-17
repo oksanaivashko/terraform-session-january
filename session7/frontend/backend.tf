3b027e83432e940d61a05ad63001597f63d62429
oksanaivashko <oksivashko13@gmail.com>
2023-02-16 19:31:22 -0600

    testing

--- session7/frontend/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-session-september-backend-oksana"
-    region         = "us-west-2"
-    key            = "session-7/backend/terrafrom.rfstate"
-    dynamodb_table = "terraform-session-sep-state-lock"
-  }
-}
--- session7/frontend/main.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_sqs_queue" "main" {
-  count = 3 
-  name  = replace(local.name, "resource", "sqs-${count.index}")
-  tags  = merge(local.common_tags, { Name = replace(local.name, "resource", "sqs-${count.index}") })
-}
