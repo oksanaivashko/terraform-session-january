b719cd3aa6ed61d71d233168ac1773314d719dc3
oksanaivashko <oksivashko13@gmail.com>
2023-02-20 19:08:55 -0600

    added index.html

--- session8/main.tf
+++ session8/main.tf
@@ -4,7 +4,21 @@ resource "aws_instance" "first_ec2" {
   key_name = aws_key_pair.terraform_server.key_name
   vpc_security_group_ids = [aws_security_group.main.id]
 
+provisioner "file" {
+  source = "home/ec2-user/terraform-sesion-january/session8/index.html" #path on terraform servwer
+  destination = "tmp/index.html"  #path for the remote server
+
+  connection {
+    type = "ssh"
+    user = "ec2-user"
+    host = self.public_ip
+    private_key = file("~/.ssh/id_rsa")
+  }
 }
+}
+
+
+
 resource "aws_key_pair" "terraform_server" {
   key_name = "Terraform-Server-Key"
   public_key = file("~/.ssh/id_rsa.pub")
