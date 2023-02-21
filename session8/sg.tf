d4fa21b3adc32b25e1a1fc23ac4fbb014694278c
oksanaivashko <oksivashko13@gmail.com>
2023-02-20 18:43:29 -0600

    added main.tf

--- session8/sg.tf
+++ session8/sg.tf
@@ -9,10 +9,10 @@ resource "aws_security_group" "main" {
     cidr_blocks      = ["0.0.0.0/0"] 
   }
   ingress {
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp" 
-    cidr =  ["0.0.0.0/0"]
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"  
+    cidr_blocks      = ["0.0.0.0/0"] 
   }
    egress {
     from_port        = 0
