f324cad6405ba463d1834bd23da93bee3d7f7868
oksanaivashko <oksivashko13@gmail.com>
2023-02-22 19:41:08 -0600

    calling module

---
+++ session9/dev/main.tf
@@ -0,0 +1,16 @@
+# Terraform consist of child and root module 
+# 1. Child module = declare the resource
+# 2. Root module where you run terraform command.
+# 
+# Root mosule = call the child module 
+# Local  module = moved is in your filesystem
+
+module "ec2_instance" {
+  source = "../../modules/ec2" # where terraform child is sitting
+}
+
+########################## Varialbes ###########################
+
+env = "dev"
+ami = "ami-0f1a5f5ada0e7da53"
+instance_type = "t2.micro"
---
+++ session9/dev/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+    region = "us-west-2"
+}
+
+#  we need a rovider for terraform to know what kind if system we need to interact 
