d697b4aab2f64cc57fd531a30fa79c697e7ed69e
oksanaivashko <oksivashko13@gmail.com>
2023-02-25 11:42:49 -0600

    testign child module

--- session10/main.tf
+++ session10/main.tf
@@ -1,6 +1,15 @@
 module "ec2" {
-  source = "github.com/oksanaivashko/terraform-session-january/tree/main/modules/ec2"
+  source = "github.com/oksanaivashko/terraform-session-january/main/modules/ec2"
   env = "dev"
   ami = "ami-06e85d4c3149db26a"
   instance_type = "t2.micro"
 }
+
+### URL Structure
+#  github.com = Domain Name (SCM)
+#  /oksanaivashko = USER
+#  /terraform-session-january = Repository
+#  /tree = 
+#  /main = branch
+#  /modules = subdirectory
+#  /ec2 = subdirectory
